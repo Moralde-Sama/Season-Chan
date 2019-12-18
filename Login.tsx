4ef4774287ba486ae60ba7d2290f4376c289f7e9
Moralde-Sama <moralde.sama@gmail.com>
2019-12-18 22:23:37 +0800

    React Native Files

---
+++ Login.tsx
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import { View, StyleSheet, ImageBackground, Text, Platform, Image } from 'react-native';
+import { LoginForm } from './components/login/LoginForm';
+import { createAppContainer, NavigationScreenProp } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import App from './App';
+
+export interface LoginProps {
+  navigation: NavigationScreenProp<any,  any>;
+}
+
+export interface LoginState {
+  username: string;
+}
+
+class Login extends React.Component<LoginProps, LoginState> {
+  constructor(props: LoginProps) {
+    super(props);
+    this.state = {
+      username: ''
+    }
+  }
+
+  private usernameInputHandler = (text: string) => {
+    this.setState(() => ({
+      username: text
+    }));
+  }
+
+  private guestClickHandler = () => {
+    this.props.navigation.navigate('Hermes');
+  }
+
+  private PoweredByAL: React.SFC = () => {
+    return (
+      <View style={styles.container_powered_by}>
+        <Text style={styles.text_powered_by}>
+          Powered by
+          <Image style={styles.anilist_logo} source={require('./assets/images/anilist.png')} />
+        </Text>
+        
+      </View>
+    );
+  } 
+ 
+  public render() {
+    return (
+      <ImageBackground 
+        source={{uri: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx108489-y4rW0W1fvto6.jpg'}} 
+        style={styles.image_background}
+      >
+        <View style={styles.bg_overlay}>
+          <View style={{flex: 2, justifyContent: "center", alignItems: "center"}}>
+            <Image style={styles.season_chan_logo} source={require('./assets/images/Season-ChanLogo.png')} />
+          </View>
+          <LoginForm 
+            usernameInputHandler={this.usernameInputHandler} 
+            usernameVal={this.state.username}
+            guestClickHandler={this.guestClickHandler} />
+          <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <this.PoweredByAL />
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+const Navigator = createStackNavigator({
+  Home: {
+    screen: Login,
+    navigationOptions: {
+      headerShown: false
+    }
+  },
+  Hermes: {
+    screen: App,
+    navigationOptions: {
+      headerShown: false
+    }
+  }
+});
+
+export default createAppContainer(Navigator);
+
+const styles = StyleSheet.create({
+    image_background: {
+      height: '100%',
+      width: '100%',
+      
+    },
+    bg_overlay: {
+      height: '100%',
+      width: '100%',
+      backgroundColor: 'rgba(0, 0, 0, .1)'
+    },
+    container_powered_by: {
+      borderRadius: 10,
+      width: 200,
+      paddingBottom: 10,
+      backgroundColor: 'rgba(0, 0, 0, .3)'
+    },
+    text_powered_by: {
+      fontSize: 20,
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      ...Platform.select({
+        android: {
+          fontFamily: 'Roboto',
+        }
+      }),
+    },
+    season_chan_logo: {
+      width: 180,
+      height: 180,
+    },
+    anilist_logo: {
+      width: 30,
+      height: 30
+    }
+});
---
+++ components/login/LoginForm.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { View, TextInput, StyleSheet, Text, Button, Platform } from 'react-native';
+
+export interface LoginFormProps {
+  usernameInputHandler: (text: string) => void;
+  usernameVal: string;
+  guestClickHandler: () => void;
+}
+
+export class LoginForm extends React.Component<LoginFormProps, any> {
+  constructor(props: LoginFormProps) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <View style={styles.background}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="white"
+          onChangeText={this.props.usernameInputHandler.bind(this)}
+          value={this.props.usernameVal}
+        />
+        
+        <TextInput
+          style={[styles.input, styles.margin_top]}
+          placeholder="Password"
+          placeholderTextColor="white"
+          secureTextEntry={true}
+          onChangeText={this.props.usernameInputHandler.bind(this)}
+          value={this.props.username_input_val}
+        />
+
+        <View style={styles.margin_top}>
+          <Button 
+            title="Login"
+            onPress={() => 'test'}
+            
+          />
+        </View>
+        <View style={{marginTop: 8}}>
+          <Button 
+            title="Guest"
+            onPress={this.props.guestClickHandler}
+            
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "stretch",
+    padding: 20,
+    backgroundColor: "rgba(0,0,0, .3)",
+    marginRight: 20,
+    marginLeft: 20,
+    borderRadius: 20
+  },
+  input: {
+    fontSize: 20,
+    color: 'white',
+    height: 40,
+    borderBottomColor: "white",
+    borderBottomWidth: 2,
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto',
+      }
+    }),
+  },
+  margin_top: {
+    marginTop: 20
+  }
+});
